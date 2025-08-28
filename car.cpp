5c69cab5190dab6b70700983a281e1b8f508dd32
Denny liu <592998069@qq.com>
2025-08-28 21:58:13 +0800

    merge

--- car.cpp
+++ car.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <iomanip>
 using namespace std;
 
 class CTyre
@@ -9,39 +8,33 @@ private:
     int radius, width;
 
 public:
-    CTyre();
-    CTyre(int radius, int width);
-    int getRadius();
-    int getWidth();
-    void setRadius(int radius);
-    void setWidth(int width);
+    CTyre()
+    {
+        radius = 0;
+        width = 0;
+    };
+    CTyre(int radius, int width)
+    {
+        this->radius = radius;
+        this->width = width;
+    };
+    int getRadius()
+    {
+        return radius;
+    };
+    int getWidth()
+    {
+        return width;
+    };
+    void setRadius(int radius)
+    {
+        this->radius = radius;
+    };
+    void setWidth(int width)
+    {
+        this->width = width;
+    };
 };
-CTyre::CTyre()
-{
-    this->radius = 0;
-    this->width = 0;
-}
-CTyre::CTyre(int radius, int width)
-{
-    this->radius = radius;
-    this->width = width;
-}
-int CTyre::getRadius()
-{
-    return radius;
-}
-int CTyre::getWidth()
-{
-    return width;
-}
-void CTyre::setRadius(int radius)
-{
-    this->radius = radius;
-}
-void CTyre::setWidth(int width)
-{
-    this->width = width;
-}
 class CCar
 {
 private:
@@ -49,51 +42,44 @@ private:
     CTyre ctyre;
 
 public:
-    CCar();
-    CCar(int price, CTyre ctyre);
-    int getPrice();
-    CTyre getCTyre();
-    int getRadius();
-    int getWidth();
-    void setPrice(int price);
-    void setCTyre(CTyre ctyre);
+    CCar()
+    {
+        price = 0;
+    };
+    CCar(int price, int radius, int width) : price(price), ctyre(radius, width) {};
+    int getPrice()
+    {
+        return price;
+    };
+    CTyre getCTyre()
+    {
+        return ctyre;
+    };
+    int getRadius()
+    {
+        return ctyre.getRadius();
+    };
+    int getWidth()
+    {
+        return ctyre.getWidth();
+    };
+    void setPrice(int price)
+    {
+        this->price = price;
+    };
+    void setCTyre(CTyre ctyre)
+    {
+        this->ctyre = ctyre;
+    };
 };
-CCar::CCar()
-{
-    this->price = 0;
-    this->ctyre.setRadius(0);
-    this->ctyre.setWidth(0);
-}
-CCar::CCar(int price, CTyre ctyre)
-{
-    this->price = price;
-    this->ctyre = ctyre;
-}
-int CCar::getPrice()
-{
-    return price;
-}
-int CCar::getRadius()
-{
-    return this->ctyre.getRadius();
-}
-int CCar::getWidth()
-{
-    return this->ctyre.getWidth();
-}
-CTyre CCar::getCTyre()
-{
-    return this->ctyre;
-}
 int main()
 {
     int price, radius, width;
     cin >> price >> radius >> width;
-    CTyre ctyre(radius, width);
-    CCar car(price, ctyre);
-    cout << "Price=" << car.getPrice() << " " << "Radius=" << car.getCTyre().getRadius() << " " << "width=" << car.getCTyre().getWidth() << endl;
+    CCar car(price, radius, width);
+    cout << "Price=" << car.getPrice() << " " << "CTyre.Radius=" << car.getCTyre().getRadius() << " " << "CTyre.Width=" << car.getCTyre().getWidth() << endl;
 
     CCar car1;
-    cout << "Price=" << car1.getPrice() << " " << "Radius=" << car1.getCTyre().getRadius() << " " << "width=" << car1.getCTyre().getWidth() << endl;
+    cout << "Price=" << car1.getPrice() << " " << "CTyre.Radius=" << car1.getCTyre().getRadius() << " " << "CTyre.Width=" << car1.getCTyre().getWidth() << endl;
     return 0;
 }
